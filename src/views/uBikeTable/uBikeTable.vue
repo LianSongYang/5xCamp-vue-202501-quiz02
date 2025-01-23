ab9452df7ecfb5b9d26a3f965853a6aee6599e05
Lian-Song YANG <lian-songyang@Lian-Songdebijixingdiannao.local>
2025-01-23 12:10:49 +0800

    Pagination

--- src/views/uBikeTable/uBikeTable.vue
+++ src/views/uBikeTable/uBikeTable.vue
@@ -143,29 +143,12 @@ const keywordsHighlight = (text, keyword) => {
   </div>  
 
   <!-- 頁籤 -->
-  <nav v-if="pagerEnd > 0">
-    <ul class="pagination">
-
-      <li @click.prevent="setPage(1)" class="page-item">
-        <a class="page-link" href>第一頁</a>
-      </li>
-      <li @click.prevent="setPage(currentPage - 1)" class="page-item">
-        <a class="page-link" href>&lt;</a>
-      </li>
-
-      <li v-for="i in pagerEnd" :class="{ active: i + pagerAddAmount === currentPage }" :key="i"
-        @click.prevent="setPage(i + pagerAddAmount)" class="page-item">
-        <a class="page-link" href>{{ i + pagerAddAmount }}</a>
-      </li>
-
-      <li @click.prevent="setPage(currentPage + 1)" class="page-item">
-        <a class="page-link" href>&gt;</a>
-      </li>
-      <li @click.prevent="setPage(totalPageCount)" class="page-item">
-        <a class="page-link" href>最末頁</a>
-      </li>
-    </ul>
-  </nav>
+  <Pagination
+    :currentPage="currentPage"
+    :totalPageCount="totalPageCount"
+    @update:currentPage="setPage"
+  />
+  
 </template>
 
 <style lang="scss" scoped>
