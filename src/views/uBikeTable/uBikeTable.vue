f7cff5b282191051ac224856a5ed321e2effa844
Lian-Song YANG <lian-songyang@Lian-Songdebijixingdiannao.local>
2025-01-23 11:41:40 +0800

    Move the uBikeTable

--- src/views/uBikeTable/uBikeTable.vue
+++ src/views/uBikeTable/uBikeTable.vue
@@ -9,6 +9,7 @@ import { ref, computed, watch } from 'vue';
 
 
 import SearchText from './components/Search.vue';
+import uBikeTable from './components/uBikeTable.vue';
 
 
 // 欄位說明:
@@ -132,51 +133,13 @@ const keywordsHighlight = (text, keyword) => {
     <SearchText v-model:searchText="searchText"/>
 
 
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th @click="setSort('sno')">
-            #
-            <span v-show="currentSort === 'sno'">
-              <i class="fa" :class="isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc'" aria-hidden="true"></i>
-            </span>
-          </th>
-          <th>
-            場站名稱
-          </th>
-          <th>
-            場站區域
-          </th>
-          <th @click="setSort('available_return_bikes')" class="pointer">
-            目前可用車輛
-            <span v-show="currentSort === 'available_return_bikes'">
-              <i class="fa" :class="isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc'" aria-hidden="true"></i>
-            </span>
-          </th>
-          <th @click="setSort('total')" class="pointer">
-            總停車格
-            <span v-show="currentSort === 'total'">
-              <i class="fa" :class="isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc'" aria-hidden="true"></i>
-            </span>
-          </th>
-          <th>
-            資料更新時間
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- 替換成 slicedUbikeStops -->
-        <tr v-for="s in slicedUbikeStops" :key="s.sno">
-          <td>{{ s.sno }}</td>
-          <td v-html="keywordsHighlight(s.sna, searchText)"></td>
-          <td>{{ s.sarea }}</td>
-          <td>{{ s.available_return_bikes }}</td>
-          <td>{{ s.total }}</td>
-          <td>{{ (s.mday) }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <!-- uBikeTable 元件, 用 prop 示範 -->
+    <uBikeTable 
+      :slicedUbikeStops="slicedUbikeStops"
+      :searchText="searchText"
+    />
+  
+  </div>  
 
   <!-- 頁籤 -->
   <nav v-if="pagerEnd > 0">
